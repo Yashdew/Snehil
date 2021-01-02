169c9436b605402b9ed969fe0470bc7951503043
Yashdew <yashdewangan123456@gmail.com>
2021-01-02 23:37:21 +0530

    new Look

---
+++ templates/js/rev.slider.js
@@ -0,0 +1,88 @@
+
+var dzrevapi;
+var dzQuery =jQuery;
+
+function dz_rev_slider_1(){
+	
+	if(dzQuery("#rev_slider_1175_1").revolution == undefined){
+		revslider_showDoubleJqueryError("#rev_slider_1175_1");
+	}else{
+		dzrevapi = dzQuery("#rev_slider_1175_1").show().revolution({
+			sliderType:"standard",
+			jsFileLocation:"plugins/revolution/v5.4.3/js/",
+			sliderLayout:"auto",
+			dottedOverlay:"none",
+			delay:9000,
+			navigation: {
+				keyboardNavigation:"off",
+				keyboard_direction: "horizontal",
+				mouseScrollNavigation:"off",
+				mouseScrollReverse:"default",
+				onHoverStop:"off",
+				touch:{
+					touchenabled:"on",
+					swipe_threshold: 75,
+					swipe_min_touches: 1,
+					swipe_direction: "horizontal",
+					drag_block_vertical: false
+				}
+				,
+				arrows: {
+					style:"uranus",
+					enable:true,
+					hide_onmobile:true,
+					hide_under:768,
+					hide_onleave:false,
+					tmp:'',
+					left: {
+						h_align:"left",
+						v_align:"center",
+						h_offset:20,
+						v_offset:0
+					},
+					right: {
+						h_align:"right",
+						v_align:"center",
+						h_offset:20,
+						v_offset:0
+					}
+				}
+			},
+			viewPort: {
+				enable:true,
+				outof:"pause",
+				visible_area:"80%",
+				presize:false
+			},
+			responsiveLevels:[1240,1024,778,480],
+			visibilityLevels:[1240,1024,778,480],
+			gridwidth:[1240,1024,778,480],
+			gridheight:[950,700,600,500],
+			lazyType:"none",
+			parallax: {
+				type:"mouse",
+				origo:"slidercenter",
+				speed:2000,
+				levels:[2,3,4,5,6,7,12,16,10,50,46,47,48,49,50,55],
+				type:"mouse",
+			},
+			shadow:0,
+			spinner:"off",
+			stopLoop:"off",
+			stopAfterLoops:-1,
+			stopAtSlide:-1,
+			shuffle:"off",
+			autoHeight:"off",
+			hideThumbsOnMobile:"on",
+			hideSliderAtLimit:0,
+			hideCaptionAtLimit:0,
+			hideAllCaptionAtLilmit:0,
+			debugMode:false,
+			fallbacks: {
+				simplifyAll:"off",
+				nextSlideOnWindowFocus:"off",
+				disableFocusListener:false,
+			}
+		});
+	}
+}
